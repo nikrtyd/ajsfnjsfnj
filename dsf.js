7454b15c583bc9e83b6d10a81c26830d7e55b26f
nikrtyd <nikrtyd@gmail.com>
2022-05-27 03:43:58 +0600

    newnew

---
+++ dsf.js
@@ -0,0 +1,11 @@
+let restr0x2 = x1 => -(10 / 3) * x1 + 10;
+let restr1x2 = x1 => x1 + 5;
+let restr2 = x2 => 2;
+let restr3x2 = x1 => 10 - x1;
+
+let granicax1 = x1 => 0;
+let granicax2 = x2 => 0;
+
+
+ // https://iamsjy17.github.io/plotta.js-page/
+ // https://github.com/iamsjy17/Plotta.js
